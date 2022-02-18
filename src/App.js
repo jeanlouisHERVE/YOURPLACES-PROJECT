76bb5eba7b62e07e8759a19346f494e8570259c8
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-02-18 08:12:28 +0100

    creation of a token expiration date

--- src/App.js
+++ src/App.js
@@ -18,10 +18,12 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token}))
+    //object with new date + 1h
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token, expiration: tokenExpirationDate.toISOString}))
   }, []);
 
   const logout = useCallback(() => {
@@ -33,8 +35,8 @@ const App = () => {
     // useEffect render just after the page loads
     useEffect(() => {
       const storedData = JSON.parse(localStorage.getItem('userData'));
-      if (storedData && storedData.token) {
-        login(storedData.userId, storedData.token)
+      if (storedData && storedData.token && new Date(storedData.expiration) > new Date() ) {
+        login(storedData.userId, storedData.token, new Date(storedData.expiration))
       }
     }, [login]); 
 
